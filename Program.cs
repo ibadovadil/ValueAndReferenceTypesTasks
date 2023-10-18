18b5aee8a313c3d2bdfdc38250255a78a6d63d00
Adil <adilibadov654@gmail.com>
2023-10-19 01:24:06 +0400

    Tasks added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ValueAndReferenceTypesTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = { 1, 2, -8, -23, 23, 57, 56, 92, 4, 2 };
+            TurnPositive(Array);
+
+        }
+        static void TurnPositive(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    array[i] *= -1;
+                }
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
